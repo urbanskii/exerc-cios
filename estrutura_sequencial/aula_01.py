51207168ea19c72408b97533eee695e81e2df4be
marcos <marcos.urbanski@gmail.com>
2020-04-15 18:47:29 -0300

    close #1

---
+++ estrutura_sequencial/aula_01.py
@@ -0,0 +1,9 @@
+def show_msg(msg):
+    """Modulo que retorna mensagem Olá Mundo!
+    :param msg: mensagem recebida
+    """
+    return print(msg)
+
+
+if __name__ == '__main__':
+    show_msg('Olá Mundo!')
---
+++ estrutura_sequencial/executa_aula_01.py
@@ -0,0 +1,3 @@
+from estrutura_sequencial.aula_01 import show_msg as sh
+
+print(sh('Olá Mundo!'))
