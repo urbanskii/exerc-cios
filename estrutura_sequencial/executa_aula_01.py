8801e1ae7194cf1b631b38e9bf4c15c68ca23a8d
marcos <marcos.urbanski@gmail.com>
2020-04-15 18:52:49 -0300

    close #1

--- estrutura_sequencial/executa_aula_01.py
+++
@@ -1,3 +0,0 @@
-from estrutura_sequencial.aula_01 import show_msg as sh
-
-print(sh('Olá Mundo!'))
---
+++ estrutura_sequencial/exercicio_aula_01.py
@@ -0,0 +1,3 @@
+from estrutura_sequencial.exercicio_01 import show_msg as sh
+
+print(sh('Olá Mundo!'))
